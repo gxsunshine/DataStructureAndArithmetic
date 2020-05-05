273209a10fa170be118bca74b7ac0215e97f71b7
gxsunshine <375870381@qq.com>
2020-05-06 00:08:30 +0800

    LeetCode200 - 岛屿的数量- BFS实现 - 有bug(队列会满)

--- src/main/java/com/gx/arithmetic/linkedListRelated/LeetCode200.java
+++ src/main/java/com/gx/arithmetic/linkedListRelated/LeetCode200.java
@@ -8,6 +8,9 @@ package com.gx.arithmetic.linkedListRelated;
  *    这样感染递归也就更快了
  */
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * @Author: gx
  * @Date: Created in 2020/5/5 21:49
@@ -30,6 +33,7 @@ public class LeetCode200 {
         };
     }
 
+    // 感染函数，DFS遍历
     public void DFSGrid(char[][] grid, int i, int j){
         // 如果超出边界之外，直接返回, 或者该位置不是岛屿'1'
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length
@@ -64,6 +68,31 @@ public class LeetCode200 {
 //        }
     }
 
+    // 感染函数 - BFS遍历
+    public void BFSGrid(char[][] grid, int i, int j){
+        ArrayBlockingQueue<Location> queue =
+                new ArrayBlockingQueue<Location>(grid.length * grid[0].length);
+//                new ArrayBlockingQueue<Location>(1, true);
+        queue.add(new Location(i, j));
+        while(!queue.isEmpty()) {
+            Location location = queue.poll();
+            // 给访问了的位置标志为 ’2' ,避免重复访问
+            grid[location.row][location.col] = '2';
+            if ((location.col + 1) < grid[0].length && grid[location.row][location.col + 1] == '1') {
+                queue.add(new Location(location.row, location.col + 1));
+            }
+            if ((location.row + 1) < grid.length && grid[location.row + 1][location.col] == '1') {
+                queue.add(new Location(location.row + 1, location.col));
+            }
+            if ((location.col - 1) > -1 && grid[location.row][location.col - 1] == '1') {
+                queue.add(new Location(location.row, location.col - 1));
+            }
+            if ((location.row - 1) > -1 && grid[location.row - 1 ][location.col] == '1') {
+                queue.add(new Location(location.row - 1, location.col));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LeetCode200 leetCode200 = new LeetCode200();
         leetCode200.initGrid();
@@ -74,10 +103,22 @@ public class LeetCode200 {
                     // 给访问了的位置标志为 ’2' ,避免重复访问
 //                    leetCode200.grid[i][j] = '2';
                     numIsLands++;
-                    leetCode200.DFSGrid(leetCode200.grid, i, j);
+                    // DFS 遍历
+//                    leetCode200.DFSGrid(leetCode200.grid, i, j);
+                    // BFS 遍历
+                    leetCode200.BFSGrid(leetCode200.grid, i, j);
                 }
             }
         }
         System.out.println("岛屿数为：" + numIsLands);
+    }
+}
+
+class Location{
+    int row;
+    int col;
+    public Location(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 }
